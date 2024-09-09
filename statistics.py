4485a510d765bc4ca788f1bb05a4fdabe057f983
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-09 14:16:51 +0700

    all tests should pass

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+""" This module contain functions often used in statistics """
 from math import sqrt
 
 
@@ -9,17 +10,18 @@ def average(data):
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """
+    The population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
-    This is different from the Python library function statistics.variance
+    This is different from the Python library function statistics.Variance
     which returns the sample variance, where the sum is divided by (n-1).
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
-           Must contain at least one element.
+    :param data: list of numbers for which variance will be computed.
+    Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
 
@@ -42,4 +44,3 @@ def variance(data):
 def stddev(data):
     """the standard deviation of a list of values"""
     return sqrt(variance(data))
-
