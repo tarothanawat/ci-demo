5cead90650baace0f518d605a6edcb0e6429c558
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-09-09 14:22:22 +0700

    This is it! Please!

--- statistics.py
+++ statistics.py
@@ -11,7 +11,7 @@ def average(data):
 
 def variance(data):
     """
-    The population variance of a list of numbers in data.
+    Calculate the population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -42,5 +42,5 @@ def variance(data):
 
 
 def stddev(data):
-    """the standard deviation of a list of values."""
+    """Calculate the standard deviation of a list of values."""
     return sqrt(variance(data))
